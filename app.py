8b9227bd900cb753c9c80085b2a645c324521273
Pratheebha Karuppusamy <pratheebha11@gmail.com>
2020-10-11 19:09:20 -0400

    Modified app.py, removed sensitive info

--- app.py
+++ app.py
@@ -60,71 +60,7 @@ def main():
       st.write("OCR Text: ", ocrtext)
       st.write("Processed OCR Text: ", ' '.join([word for word in text]))
       st.subheader("Extracted entities:")
-
-      rawtext = ' '.join([val for val in ocrtext])
-      # barcode, year, genus, species, collector, geography = extract.handwrittenText(ocrtext, text)
-      year = model.findYear(text)
-      collector = model.findCollector(ocrtext)
-      geography = model.findGeography(rawtext)
-      genus, species = model.findScientificName(text)
-      st.write("Scientific Name: ", genus + " " + species)
-      st.write("Collector: ", ','.join([val for val in collector]))
-      st.write("Geography: ", ','.join([val for val in geography]))
-      st.write("Year: ", ','.join([val for val in year]))
-    
       
-
-      # entities = list(genus) + list(species) + list(collector) + list(geography)
-      # boxImage = box.generateboundingbox(image, content, entities)
-      # st.subheader("Tagged entities")
-      # st.image(boxImage, width=500, channels='BGR')
-
-
-
-
-      # rawText = ' '.join([word for word in raw])
-      # st.write("Raw OCR Text: ",rawText)
-      # cleanText = ' '.join([word for word in clean])
-      # st.write("OCR Text: ", cleanText)
-
-
-
-
-
-
-    # Extract the OCR text from the handwritten image
-
-    # if url:
-    #     image, raw, clean = ocr.handwrittenOCR(url)
-
-    #     st.image(image, width=500, channels='BGR')
-            
-    #     # Extract Entities
-    #     # model = extract.handwrittenText()
-    #     # # st.write("Barcode: ", model.findBarcode(clean))
-    #     # st.write("Scientific Name: ", model.findScientificName(clean))
-    #     # st.write("Year: ", model.findYear(clean))
-    #     # st.write("Collector: ", model.findCollector(raw))
-    #     # st.write("Geography: ", model.findGeography(raw))
-
-    #     st.write("Extracting entities from the image...")
-    #     barcode, year, genus, species, collector, geography = extract.handwrittenText(raw, clean)
-
-    #     st.write("Barcode: ", ','.join([val for val in barcode]))
-    #     st.write("Scientific Name: ", genus + " " + species)
-    #     st.write("Year: ", ','.join([val for val in year]))
-    #     st.write("Collector: ", ','.join([val for val in collector]))
-    #     st.write("Geography: ", ','.join([val for val in geography]))
-
-
-    #     # entities = barcode + list(year) + list(genus) + list(species) + list(collector) + list(geography)
-    #     # boxImage = box.generateboundingbox(url, entities)
-    #     # st.image(boxImage, width=500, channels='BGR')
-
-    #     rawText = ' '.join([word for word in raw])
-    #     st.write("Raw OCR Text: ",rawText)
-    #     cleanText = ' '.join([word for word in clean])
-    #     st.write("OCR Text: ", cleanText)    
 
 def saveImage(image_url):
   """
